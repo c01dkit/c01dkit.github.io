59ca67a0aa95c078ba1bdfa7c92fc464e7135cce
c01dkit <contact@c01dkit.com>
2025-06-10 09:53:29 +0800

    Sync raw data at Tue Jun 10 09:53:29 AM CST 2025

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-git push origin main:test
+git push origin editmode:test
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo
